517bba0cfba4341f9daa886e5f163872ed40229b
XthiagoX <thiagoaraujo43034@gmail.com>
2023-09-20 21:48:21 -0300

    <style>[header Style]: inserção do logo e estilo do header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
 import headerStyles from './Header.module.css'
+import igniteLogo from '../assets/ignite-logo.svg'
 
 export default function Header(){
     return (
         <header className={headerStyles.header}> 
+        <img src={igniteLogo} alt="Logotipo do ignite" />
         <strong >Ignite Feed</strong>
         </header>
     );
