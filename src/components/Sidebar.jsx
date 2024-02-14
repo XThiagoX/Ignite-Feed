347f73e597727adcd3969f0a98b7d4677da2e6e6
Thiago Araujo <thiagoaraujo43034@gmail.com>
2024-02-14 15:08:29 -0300

    <style/feat/chore/docs>[Sidebar]: Finalização de estilização e funcionalidades da sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import Avatar from './Avatar';
 import styleSidebar from './Sidebar.module.css'
 import { PencilLine } from "phosphor-react";
 
@@ -10,7 +11,7 @@ export default function Sidebar(){
             src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8ZGV2ZWxvcG1lbnQlMjB3ZWJ8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=400&q=50"
          />
         <div className={styleSidebar.profile}>
-            <img className={styleSidebar.avatar} src="https://avatars.githubusercontent.com/u/59093773?v=4" alt="" />
+            <Avatar src="https://github.com/XThiagoX.png" />
             <strong>Thiago Araujo</strong>
             <span>Web Developer</span>
             
