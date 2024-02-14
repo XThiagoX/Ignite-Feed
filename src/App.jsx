4b62cede4e7f958bc0f5d3ab1eaaaf605131bc42
Thiago Araujo <thiagoaraujo43034@gmail.com>
2024-02-14 15:13:31 -0300

    <style/feat>[App]: Definação estática das infomações do post com array, e definição da propriedades do Post

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,37 @@ import styles from './App.module.css'
 
 import './global.css'
 
+const posts =  [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/xthiagox.png',
+      name: 'Thiago Araujo',
+      role: 'Web Developer'
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',},      
+      {type: 'link', content: 'https://github.com/XThiagoX'},
+    ],
+    publishedAt:  new Date ('2024-05-03 17:06:00') 
+  },
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      role: 'Desenvolvdor Roket'
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',},      
+      {type: 'link', content: 'https://github.com/maykbrito'},
+    ],
+    publishedAt:  new Date ('2022-01-03 17:06:00') 
+  }
+]
+
 function App() {
   return (
     <div>
@@ -13,14 +44,17 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post 
-          author="Thiago Araujo"
-          content="Minhas novas habilidades"
-          />     
-          <Post 
-          author="Felipe Flanderz"
-          content="Novas Feramentas e Skills"
-          /> 
+          {
+            posts.map( posts => {
+            return (
+              <Post
+                key={posts.id}
+                author={posts.author}
+                content={posts.content}
+                publishedAt={posts.publishedAt}
+              />
+              )
+          })}
         </main>
       </div>
 
