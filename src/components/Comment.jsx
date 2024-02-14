783188a93c7fe862a81f31f5174aa279221d41ce
Thiago Araujo <thiagoaraujo43034@gmail.com>
2024-02-14 15:25:23 -0300

    <feat>[Comentários]:Definição da proproidades de comentério, Aplaudir e excluir comentário.

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,26 @@
 import { ThumbsUp, Trash } from 'phosphor-react'
 import styles from './Comment.module.css'
+import Avatar from './Avatar'
+import { useState } from 'react'
 
-export default function Comment(){
+
+export default function Comment({content, onDeleteComment}){
+     const [likeCount, setLikeCount] = useState(0);
+
+    function handleDeleteComment(){
+        onDeleteComment(content)
+    }  
+
+    function handleLikeComment(){
+        console.log(likeCount)
+        setLikeCount((likeCountState) => {
+            return likeCountState + 1 
+        });
+    }
+    
     return(
         <div className={styles.comment}>
-            <img src="https://github.com/XThiagoX.png" alt="" />
+            <Avatar hasBorder={false} src="https://github.com/XThiagoX.png" alt="" />
             
             <div className={styles.commentBox}>
                 <div className={styles.commentContent}>
@@ -13,16 +29,16 @@ export default function Comment(){
                             <strong>Thiago Pereira</strong>
                             <time title='10 de outubro de 2023' dateTime=''>Publicado há 1 hora atrás</time>                                
                         </div>
-                        <button title='Deletat comentário'>
+                        <button onClick={handleDeleteComment} title='Deletat comentário'>
                             <Trash size={24}/>
                         </button>
                     </header> 
-                    <p>Muito bom thiago, parabéns seu lindo</p>
+                    <p> {content} </p>
                 </div>
                 <footer>
-                    <button>
+                    <button onClick={handleLikeComment}>
                         <ThumbsUp/>
-                        Aplaudir <span>20</span>
+                        Aplaudir <span> {likeCount} </span>
                     </button>
                 </footer>
             </div>
